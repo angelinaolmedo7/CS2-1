c22ef0868444c99558bca1058f6c2751a16ffb02
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-12-01 22:50:13 -0800

    time complexitites recursive

--- sorting_recursive.py
+++ sorting_recursive.py
@@ -5,8 +5,8 @@ import sorting_iterative
 def merge(items1, items2):
     """Merge given lists of items, each assumed to already be in sorted order,
     and return a new list containing all items in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n) where n is the length of both lists combined
+    TODO: Memory usage: O(n) because I made a new list for this, you could probably do it in place though"""
     
     # TODO: Repeat until one list is empty
     # TODO: Find minimum item in both lists and append it to new list
@@ -28,8 +28,8 @@ def split_sort_merge(items):
     """Sort given items by splitting list into two approximately equal halves,
     sorting each with an iterative sorting algorithm, and merging results into
     a list in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O of whatever the iterative algorithm you used is :/
+    TODO: Memory usage: O(1)"""
     # TODO: Split items list into approximately equal halves
     # TODO: Sort each half using any other sorting algorithm
     # TODO: Merge sorted halves into one list in sorted order
@@ -54,19 +54,20 @@ def split_sort_merge(items):
 def merge_sort(items):
     """Sort given items by splitting list into two approximately equal halves,
     sorting each recursively, and merging results into a list in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n log(n))
+    TODO: Memory usage: O(n)"""
+    
     # TODO: Check if list is so small it's already sorted (base case)
-    # TODO: Split items list into approximately equal halves
-    # TODO: Sort each half by recursively calling merge sort
-    # TODO: Merge sorted halves into one list in sorted order
-
     if len(items) <= 1:
         return(items)
+
+    # TODO: Split items list into approximately equal halves
     half = len(items)//2
+    
     first_half = items[:half]
     second_half = items[half:]
 
+    # TODO: Merge sorted halves into one list in sorted order
     merge_sort(first_half)
     merge_sort(second_half)
 
@@ -78,8 +79,8 @@ def partition(items, low, high):
     `[low...high]` by choosing a pivot (TODO: document your method here) from
     that range, moving pivot into index `p`, items less than pivot into range
     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n), just goes through once
+    TODO: Memory usage: O(1), in place comparisons"""
     # TODO: Choose a pivot any way and document your method in docstring above
     # pivot is gonna be the last index. p is then len(items[low:high])-1
 
@@ -106,9 +107,9 @@ def partition(items, low, high):
 def quick_sort(items, low=None, high=None):
     """Sort given items in place by partitioning items in range `[low...high]`
     around a pivot item and recursively sorting each remaining sublist range.
-    TODO: Best case running time: O(n log(n)), when len(items) is a power of 2, as each subset needs to be split the same amount of times
-    TODO: Worst case running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Best case running time: O(n log(n))
+    TODO: Worst case running time: O(n^2)
+    TODO: Memory usage: O(log(n))"""
     # TODO: Check if high and low range bounds have default values (not given)
     if low == None or high == None:
         low = 0
